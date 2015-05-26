998df21701c004ed93fa3cc9c42d72a7c4da07ce
nil <nil.mamano@gmail.com>
2015-05-25 23:03:13 -0700

    adapted run.sh script to new problems folder

--- run.sh
+++ run.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
-f=problemas/"${1}"/
 
 if [ "$#" -eq 0 ]; then
 	echo "missing problem name"
+	exit
 fi
+
+f="problems/${1}/"
+
 if [ "$#" -eq 1 ]; then
-	./juezejecutable "${f}jp" "${f}jp2input" "${f}input2sat" "${f}propuestasolucion" "${f}validator" "${f}format"
+	fproposal="${f}proposals/correct/"
+	dbg=""
 fi
+
 if [ "$#" -eq 2 ]; then
-    ./juezejecutable "-d" "${f}jp" "${f}jp2input" "${f}input2sat" "${f}propuestasolucion" "${f}validator" "${f}format"
+	if [ "${2}" == "-d" ]; then
+		fproposal="${f}proposals/correct/"
+		dbg="-d"
+	else
+		fproposal="${f}proposals/${2}/"
+		dbg=""
+	fi
 fi
+
+if [ "$#" -eq 3 ]; then
+	fproposal="${f}proposals/${2}/"
+	dbg="-d"
+fi
+
+s="./juezejecutable ${dbg} ${f}jp ${f}jp2input ${f}input2sat ${fproposal}propuestasolucion ${f}validator ${f}format"
+echo "${s}"
+eval "${s}"
